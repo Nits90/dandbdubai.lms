3374da6f6cad59c519d2997be1b8ddf625089a4a
Nits90 <csecec.1802954@gmail.com>
2023-01-09 18:32:13 +0530

    Updates

--- src/private component/RestrictedAccess.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import cookie from "js-cookie";
-
-export const RestrictedAccess = () => {
-    const auth = cookie.get('user')
-    return auth ? <Outlet/>:<Navigate to="/login"/>
-}
